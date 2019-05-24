4042dc207db4f541169842047a1a92f07c8d6e60
NikitaSharipov <sharipovkzn@mail.ru>
2019-05-24 16:48:07 +0300

    fix

--- spec/feature/computer/booking_spec.rb
+++ spec/feature/computer/booking_spec.rb
@@ -1,1 +1,19 @@
+require 'rails_helper'
 
+feature 'User can request a software installation', %q{
+  In order to User a computer
+  As an authenticated user
+  I'd like to be able to book a computer
+} do
+
+  given(:user) { create :user }
+  given!(:computer) { create(:computer) }
+
+  background { sign_in(user) }
+
+  scenario 'User tries to book a computer' do
+    visit computer_book_path
+
+  end
+
+end
